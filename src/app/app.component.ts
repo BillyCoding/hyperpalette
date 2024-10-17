df8d974591312713eda4ec17a36be216e667c8ed
Gustavo Ferreira <gustavo.rocha.191066@hotmail.com>
2024-10-17 17:21:59 -0300

    feat: integration with thecolorapi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import {
   NgFor,
   NgStyle
 } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -17,7 +18,30 @@ export class AppComponent {
   title = 'angular-color-pallete';
 
   palleteSize = 5
-  colors: string[] = ['#ffce00', '#ff2424', '#3eff45', '#ecbF90', '#fefefe']
+  loading = false
+  colors: string[] = ['#0f172a', '#1e293b', '#334155', '#475569', '#64748b']
+
+
+  constructor(private http: HttpClient) { }
+
+  async handleGenerateRandomColorPalleteApi() {
+    if (!this.loading) {
+      this.loading = true
+      const randomColor = Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
+      const apiUrl = `https://www.thecolorapi.com/scheme?hex=${randomColor}&mode=monochrome&count=${this.palleteSize}`;
+      this.http.get<any>(apiUrl).subscribe(
+        (response) => {
+          this.colors = response.colors.map((color: any) => color.hex.value);
+        },
+        (error) => {
+          console.error('Erro ao buscar a paleta de cores:', error);
+        },
+        () => {
+          this.loading = false
+        }
+      );
+    }
+  }
 
   handleGenerateRandomColorPallete() {
     const newColors: string[] = [];
