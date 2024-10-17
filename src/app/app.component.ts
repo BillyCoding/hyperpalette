92fe309f2ea9df9f1a0a68f867416b33ff4b3dda
Gustavo Ferreira <gustavo.rocha.191066@hotmail.com>
2024-10-17 16:42:24 -0300

    feat: add color pallete generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+  NgFor,
+  NgStyle
+} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor, NgStyle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   title = 'angular-color-pallete';
+
+  palleteSize = 5
+  colors: string[] = ['#ffce00', '#ff2424', '#3eff45', '#ecbF90', '#fefefe']
+
+  handleGenerateRandomColorPallete() {
+    const newColors: string[] = [];
+
+    for (let i = 0; i < this.palleteSize; i++) {
+      const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+      newColors.push(randomColor);
+    }
+
+    this.colors = newColors
+  }
 }
