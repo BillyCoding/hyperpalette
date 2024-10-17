848132932821035f8681f1c6751e716a7b50eb01
Gustavo Ferreira <gustavo.rocha.191066@hotmail.com>
2024-10-17 17:47:54 -0300

    feat: change generation mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,8 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   title = 'angular-color-pallete';
 
-  palleteSize = 5
+  paletteSize = 5
+  paletteMode = 'monochrome'
   loading = false
   colors: string[] = ['#0f172a', '#1e293b', '#334155', '#475569', '#64748b']
 
@@ -28,7 +29,7 @@ export class AppComponent {
     if (!this.loading) {
       this.loading = true
       const randomColor = Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
-      const apiUrl = `https://www.thecolorapi.com/scheme?hex=${randomColor}&mode=monochrome&count=${this.palleteSize}`;
+      const apiUrl = `https://www.thecolorapi.com/scheme?hex=${randomColor}&mode=${this.paletteMode}&count=${this.paletteSize}`;
       this.http.get<any>(apiUrl).subscribe(
         (response) => {
           this.colors = response.colors.map((color: any) => color.hex.value);
@@ -46,11 +47,17 @@ export class AppComponent {
   handleGenerateRandomColorPallete() {
     const newColors: string[] = [];
 
-    for (let i = 0; i < this.palleteSize; i++) {
+    for (let i = 0; i < this.paletteSize; i++) {
       const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
       newColors.push(randomColor);
     }
 
     this.colors = newColors
+  }
+
+  handleChangeMode(event: Event) {
+    const target = event.target as HTMLSelectElement;
+    this.paletteMode = target.value
+    this.handleGenerateRandomColorPalleteApi()
   }
 }
